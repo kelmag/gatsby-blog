01ff48d5cb1cd9da5fad17bf8b9b3289244082a2
kelmag <khaliilelmaghraoui@gmail.com>
2019-04-19 20:42:43 +0100

    fix Header typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Footer from "../components/footer"
-import Header from "../components/Header"
+import Header from "../components/header"
 import layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
